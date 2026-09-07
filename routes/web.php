12c56027dd72fb9a775e2539ef33e40039d62c8d
The-estray <317675240+The-estray@users.noreply.github.com>
2026-09-07 20:35:44 +0300

    feat: create and run cars table migration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts', [Post::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
