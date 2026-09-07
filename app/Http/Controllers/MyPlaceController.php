a90a6937972ef145502fd3ce9d9db4776fff3f9e
The-estray <317675240+The-estray@users.noreply.github.com>
2026-09-07 18:43:19 +0300

    refactor: clean up routes file

--- app/Http/Controllers/MyPlaceController.php
+++ app/Http/Controllers/MyPlaceController.php
@@ -10,29 +10,5 @@ class MyPlaceController extends Controller
     {
         return 'Добро пожаловать, в бойцовский клуб Гладиатор!';
     }
-    /*
-        public function index1() 
-        {
-            return 'Добро пожаловать в бойцовский клуб5!';
-        }
-
-        public function index2() 
-        {
-            return 'Добро пожаловать в бойцовский клуб4!';
-        }
-
-        public function index3() 
-        {
-            return 'Добро пожаловать в бойцовский клуб3!';
-        }
-
-        public function index4() 
-        {
-            return 'Добро пожаловать в бойцовский клуб2!';
-        }
-
-        public function index5() 
-        {
-            return 'Добро пожаловать в бойцовский клуб1!';
-    }*/
+   
 }
--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,7 @@
 <?php
 
 use App\Http\Controllers\MyPlaceController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MyPlaceController::class, 'index']);
-
-/*
-    Route::get('/1', [MyPlaceController::class, 'index1']);
-    Route::get('/2', [MyPlaceController::class, 'index2']);
-    Route::get('/3', [MyPlaceController::class, 'index3']);
-
-    Route::get('/mypage', function() {
-        return 'This is my page';
-    });
-
-    Route::get('/home', function() {
-        return 'This is my home!';
-    });
-
-    Route::get('/bjj', function(){
-        return 'BJJ is cool!';
-    });
-
-    Route::get('/gym',function() {
-        return 'Gym isnt bad, but not cool!';
-    });
-
-    Route::get('/cardistri',function(){
-        return 'Cardistri is a cool hobby!';
-    });
-
-    Route::get('/water',function(){
-        return 'Water is the best drink!';
-    });
-
-    Route::get('/steam',function(){
-        return 'Steam is the best gameing platform in the world!';
-    });
-
-    Route::get('/epicgames',function(){
-        return 'Epic Games is total shit!';
-    });
-
-    Route::get('/pc',function(){
-        return 'Computers are the best!';
-}); */
-
+Route::get('/posts', [Post::class, 'index']);
